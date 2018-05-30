e6e4b7a175da0095b17c9342ef6c551d5f132e38
Mike <munkyu0813@gmail.com>
2018-05-29 23:19:20 -0400

    modify styles

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,7 +1,10 @@
 export const LANDING = '/';
 export const HOME = '/home';
+export const BROWSE = '/browse';
 export const ABOUT = '/about';
 export const SIGN_UP = '/signup';
 export const SIGN_IN = '/signin';
 export const ACCOUNT = '/account';
 export const PASSWORD_FORGET = '/pw-forget';
+
+export const CREATE_JAR = '/jar/create'
